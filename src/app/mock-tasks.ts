41e6af8179eea8e13e5f192884d569d818a14f35
adrani_laptop <riadh_adrani@hotmail.fr>
2021-10-09 22:44:09 +0100

    components, services

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,6 @@
+import { Task } from "./Task"
+
+export const TASKS : Task[] = [
+     {id:1,text:"Doctors Appointement",day:"May 5th at 2:30pm",reminder:true},
+     {id:2,text:"Cringy moment",day:"May 10th at 5:35pm",reminder:false},
+]
---
+++ src/app/services/task.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Task } from '../Task';
+import {HttpClient, HttpHeaders} from "@angular/common/http"
+import { TASKS } from '../mock-tasks';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private apiURL = "http://localhost:5000/tasks";
+
+  constructor(private http:HttpClient) { }
+
+  getTasks(): Observable<Task[]>{
+    return this.http.get<Task[]>(this.apiURL)
+  }
+}
